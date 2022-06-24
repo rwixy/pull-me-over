74b6d633f4ad68a00f6dd3fc4c52b100de035998
BirddMann <108105014+BirddMann@users.noreply.github.com>
2022-06-23 17:37:31 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+-- compatibility wrapper
+fx_version 'adamant'
+game 'gta5'
+
+description ''
+author 'Bird'
+version '1'
+
+client_scripts {
+   'client.lua'
+}
+
+server_script {
+   'server.lua'
+}
