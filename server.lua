b9ae33beae8b793d5120552955f13f8095241569
BirddMann <108105014+BirddMann@users.noreply.github.com>
2022-06-29 14:48:38 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,3 +2,9 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+RegisterServerEvent('pullmeover:speedingticket')
+AddEventHandler('pullmeover:speedingticket', function(src)
+	local src = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeAccountMoney('bank', 500)
+end)
