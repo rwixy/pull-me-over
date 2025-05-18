e8b07d5a758053de589bbe34ff96a9a3f4a28d83
Ihsan <122683406+rwixy@users.noreply.github.com>
2025-05-18 11:52:04 +0530

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,7 +1,3 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
 RegisterServerEvent('pullmeover:speedingticket')
 AddEventHandler('pullmeover:speedingticket', function(src)
 	local src = source
